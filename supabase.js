bf37d90198919370f697685e7d187aa413169ffe
birdsin <146730536+birdsin@users.noreply.github.com>
2026-08-28 21:10:53 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -3,7 +3,7 @@
 // Browser-safe ES module
 // =====================================================
 
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
 const SUPABASE_URL =
   "https://vjlitqujcujwsislprfg.supabase.co";
