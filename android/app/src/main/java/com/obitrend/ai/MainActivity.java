ade90e598afc31ebdb4c686983e1629ff4c574ce
birdsin <146730536+birdsin@users.noreply.github.com>
2026-09-07 14:54:14 +0100

    Enable AdMob rewarded plugin
    Override onCreate method to register AdMobRewardedPlugin.

--- android/app/src/main/java/com/obitrend/ai/MainActivity.java
+++ android/app/src/main/java/com/obitrend/ai/MainActivity.java
@@ -1,5 +1,12 @@
+
 package com.obitrend.ai;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(android.os.Bundle savedInstanceState) {
+        registerPlugin(AdMobRewardedPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
