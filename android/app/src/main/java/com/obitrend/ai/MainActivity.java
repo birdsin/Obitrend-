1b04a298405a1b17d019f72bd814118746c7bc52
birdsin <146730536+birdsin@users.noreply.github.com>
2026-09-08 09:33:08 +0100

    Update MainActivity.java

--- android/app/src/main/java/com/obitrend/ai/MainActivity.java
+++ android/app/src/main/java/com/obitrend/ai/MainActivity.java
@@ -1,12 +1,15 @@
-
 package com.obitrend.ai;
 
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+
     @Override
     public void onCreate(android.os.Bundle savedInstanceState) {
+
         registerPlugin(AdMobRewardedPlugin.class);
+        registerPlugin(ObitrendBiometricPlugin.class);
+
         super.onCreate(savedInstanceState);
     }
 }
